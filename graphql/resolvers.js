ff229394c94105d391cbc9cb60f22b4743037fbb
baejinnwoo <bjwkor@naver.com>
2020-01-28 16:26:44 +0900

    API를 이용해서 쿼리

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,15 +1,11 @@
 //Query를 resolve 
-import {getById,getMovies,addMovie, deleteMovie} from "./db";
+//import {getById,getMovies,addMovie, deleteMovie} from "./db";
+import {getMovies} from "./db2";
 
 
 const  resolvers= {
     Query:{
-        movies: () => getMovies() ,
-        movie: (_,{id}) => getById(id)    
-    },
-    Mutation:{
-        addMovie: (_,{movieName,score}) => addMovie(movieName,score),
-        deleteMovie : (_,{id}) => deleteMovie() 
+        movies: (_,{limit,rating}) => getMovies(limit,rating)
     }
 };
 
